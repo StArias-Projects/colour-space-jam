02e505b80b27da7cd6faf8527215e380723dbbb4
jackv471 <96673252+jackv471@users.noreply.github.com>
2025-03-29 21:02:30 +1100

    Added masking over sprites to indicate health

--- macro-jam/Assets/Scripts/Enemies/EnemyController.cs
+++ macro-jam/Assets/Scripts/Enemies/EnemyController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using System.Linq;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class EnemyController : MonoBehaviour
 {
@@ -10,6 +12,12 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField]
     private EnemyWeaponController weaponController;
+
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    [SerializeField]
+    private SpriteRenderer shadowSprite;
 
     [SerializeField]
     [Min(0)]
@@ -119,12 +127,26 @@ public class EnemyController : MonoBehaviour
     public void ReceiveDamage(float damage)
     {
         health -= damage;
+
+        Tween tween = sprite.material.DOFloat(health / maxHealth, "_PercentHealth", .8f).SetEase(Ease.OutBounce);
+        shadowSprite.material.DOFloat(health / maxHealth, "_PercentHealth", .8f).SetEase(Ease.OutBounce);
+
         if (health <= 0)
         {
             health = 0;
-            enemyManager.ResetEnemy(this);
+            ReleaseBackToPoolAfter(tween.Duration());
         }
     }
+
+    //i did this so the animation of the tween can play out before it is released back to pool, might need to disable collision while it is dying
+    async Task ReleaseBackToPoolAfter(float seconds)
+    {
+        await Awaitable.WaitForSecondsAsync(seconds);
+        enemyManager.ResetEnemy(this);
+    }
+
+
+
 
     public void StartEnemy(Vector2 position)
     {
